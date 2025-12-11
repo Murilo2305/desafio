c5b746d93145991910682b90e9137fec6330ad0b
murilo2305 <murilo.santos.silva.v@gmail.com>
2025-12-11 11:19:13 -0300

    Fetchs adicionados como comentratios em madeira.js

--- web-data-viz/src/routes/madeira.js
+++ web-data-viz/src/routes/madeira.js
@@ -12,3 +12,40 @@ router.get('/obterFaixa/:idGalpao', function (req,res){
 });
 
 module.exports = router;
+
+// fetch('/madeira/alterarFaixa',{
+
+//     method: "POST",
+//     headers: {
+//         "Content-Type": "application/json"
+//     },
+//     body: JSON.stringify({
+//         idGalpao: Insira_algo,
+//         min: Insira_algo,
+//         max: Insira_algo,
+//         desc: Insira_algo
+//     })
+     
+// })
+// .then((resultado) => {
+
+//     console.log('Tudo Certo! Mensagem: '+ resultado);
+
+// })
+// .catch((erro) => {
+
+//     console.log('Algo deu errado! erro: '+ erro);
+
+// })
+
+// fetch(`/madeira/obterFaixa/${Insira_algo}`)
+//     .then((resultado) => {
+
+//         console.log('Tudo Certo! Mensagem: '+resultado);
+
+//     })
+//     .catch((erro) => {
+
+//         console.log('Algo deu errado!Mensagem: '+ erro);
+
+//     })
